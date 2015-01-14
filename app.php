5d5e0a756e81e0ceb27b6700367886239585f82d
james2doyle <james2doyle@gmail.com>
2015-01-14 15:40:27 -0500

    comment cleanups

--- app.php
+++ app.php
@@ -30,7 +30,7 @@ $app->post('/return_json', function () use ($app) {
 });
 
 $app->get('/cached', function () use ($app) {
-  //Cache this view for 1 day with the key "latest-cache"
+  //Cache this view with the key "latest-cache"
   $app->view->cache(array(
     'key' => 'latest-cache', // this becomes the filename
     'level' => \Phalcon\Mvc\View::LEVEL_ACTION_VIEW
