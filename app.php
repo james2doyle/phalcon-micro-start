1bf76425a318f6313014b7204dd8fe6855e6113b
james2doyle <james2doyle@gmail.com>
2015-02-22 14:04:40 -0500

    basic ACL example

--- app.php
+++ app.php
@@ -16,6 +16,40 @@ $app->get('/hello/{name}', function ($name) use ($app) {
     ));
 });
 
+$acl = new \Phalcon\Acl\Adapter\Memory();
+
+// Create some roles
+$roleAdmins = new \Phalcon\Acl\Role("Admins", "Super-User role");
+$roleGuests = new \Phalcon\Acl\Role("Guests");
+
+// Add "Guests" role to acl
+$acl->addRole($roleGuests);
+$acl->addRole($roleAdmins);
+
+// Define the "SpecialPages" resource
+$pageResource = new \Phalcon\Acl\Resource("SpecialPages");
+
+// Add "SpecialPages" resource with a couple of operations
+$acl->addResource($pageResource, "view");
+$acl->addResource($pageResource, "edit");
+
+$acl->allow("Admins", "SpecialPages", "view");
+$acl->deny("Guests", "SpecialPages", "view");
+
+$app->get('/auth', function () use ($app, $acl) {
+  $role = ($app->request->getQuery('role', 'string')) ? $app->request->getQuery('role', 'string'): "Guests";
+  $allowed = $acl->isAllowed($role, "SpecialPages", "view");
+  // warning: $acl->isAllowed returns a bool, Phalcon\Acl::ALLOW returns an int!
+  if ($allowed === true) {
+    echo $app->view->render('index/auth', array(
+      'role' => $role
+      ));
+  } else {
+    $app->response->setStatusCode(403, "Forbidden")->sendHeaders();
+    echo $app->view->render('errors/403');
+  }
+});
+
 // test: $.post('return_json', {count: 1}).done(function(res){console.log(res)})
 $app->post('/return_json', function () use ($app) {
   $template = $app->view->render('partials/form', array(
@@ -33,7 +67,7 @@ $app->get('/cached', function () use ($app) {
   //Cache this view with the key "latest-cache"
   $app->view->cache(array(
       'key' => 'latest-cache' // this becomes the filename
-    ));
+      ));
   // this will only be changed when the cache is invalidated
   $latest = time();
   echo $app->view->render('index/latest', array(
